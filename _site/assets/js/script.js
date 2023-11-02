f284fe6ad6f250583588026c6537b672d97586f0
Albert Ko <albert.ko@clio.com>
2023-11-02 17:34:01 -0400

    Add basic score counter

---
+++ _site/assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const blueScore = document.getElementById("blue-score");
+  blueScore.addEventListener("click", function () {
+    const num = parseInt(blueScore.textContent);
+    blueScore.innerHTML = num + 1;
+  });
+
+  const redScore = document.getElementById("red-score");
+  redScore.addEventListener("click", function () {
+    const num = parseInt(redScore.textContent);
+    redScore.innerHTML = num + 1;
+  });
+});
